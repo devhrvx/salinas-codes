ff0f37040c0eb5cf59ffb701f68a56b740cedeb1
hrvyCased <salinas.johnharvey11@gmail.com>
2024-09-21 18:37:40 +0800

    Base

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const swiper = new Swiper('.slider-wrapper', {
-        effect: "coverflow",
+  const swiper = new Swiper('.slider-wrapper', {
+      effect: "coverflow",
       grabCursor: true,
       centeredSlides: true,
       slidesPerView: 2,
       coverflowEffect: {
-        rotate: 15,
-        stretch: 0,
-        depth: 300,
-        modifier: 1,
-        slideShadows: true,
+          rotate: 15,
+          stretch: 0,
+          depth: 300,
+          modifier: 1,
+          slideShadows: false,
       },
       loop: true,
-
+      
       breakpoints: {
-        0: {
-            slidesPerView: 1,
-        },
-        800: {
-            slidesPerView: .67,
-        },
-        1200: {
-          slidesPerView: 3,
-        }
-    },
-      });
+          0: {
+              slidesPerView: 1,
+          },
+          800: {
+              slidesPerView: 0.67,
+          },
+          1200: {
+              slidesPerView: 3,
+          }
+      },
+  });
 });
