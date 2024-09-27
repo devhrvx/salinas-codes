854a95f60d7ecd428fd2fdceaf410fab8a8e82be
hrvyCased <salinas.johnharvey11@gmail.com>
2024-09-27 21:32:01 +0800

    sliding text cause y not?

---
+++ spray-slay/script.js
@@ -0,0 +1,18 @@
+window.addEventListener('scroll', () => {
+    const scrollPosition = window.scrollY;
+    const leftText = document.querySelector('.left');
+    const rightText = document.querySelector('.right');
+    
+    if (scrollPosition > 10) {
+        leftText.style.transform = 'translateX(0)'; 
+        rightText.style.transform = 'translateX(0)';
+        leftText.style.opacity = 1;
+        rightText.style.opacity = 1;
+    } else {
+        leftText.style.transform = 'translateX(-100%)';
+        rightText.style.transform = 'translateX(100%)';
+        leftText.style.opacity = 0;
+        rightText.style.opacity = 0;
+    }
+});
+  
