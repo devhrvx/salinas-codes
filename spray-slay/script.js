e6d0f57473497a5aede0ee10b9441d3417716241
hrvyCased <salinas.johnharvey11@gmail.com>
2024-09-29 17:53:10 +0800

    Variants slides

--- spray-slay/script.js
+++ spray-slay/script.js
@@ -1,3 +1,11 @@
+var prodInfo = {
+    name: 'Spray and Slay',
+    description: 'Multi-benefit sunscreen spray',
+    effects: 'Cooling, Whitening, Anti-aging, Anti-microbial,and UV Protection',
+    activeIngredients: 'Retinol, Niacinamide, Menthol, Titanium dioxide, Zinc oxide, Glycolic acid, Vitamin C, Aloe Vera extract,and Triclosan',
+    project: 'Physical science chemistry making a website for active ingredients powered product'
+};
+
 window.addEventListener('scroll', () => {
     const scrollPosition = window.scrollY;
     const leftText = document.querySelector('.left');
@@ -16,11 +24,64 @@ window.addEventListener('scroll', () => {
     }
 });
 
-var prodInfo = {
-    name: 'Spray and Slay',
-    description: 'Multi-benefit sunscreen spray',
-    effects: 'Cooling, Whitening, Anti-aging, Anti-microbial,and UV Protection',
-    activeIngredients: 'Retinol, Niacinamide, Menthol, Titanium dioxide, Zinc oxide, Glycolic acid, Vitamin C, Aloe Vera extract,and Triclosan',
 
+const swiper = new Swiper('.slider-wrapper', {
+    // Optional parameters
+    direction: 'horizontal',
+    spaceBetween: 30,
+    centeredSlides: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true
+    },
+    
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
 
-};
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        620: {
+            slidesPerView: 1
+        },
+        1024: {
+            slidesPerView: 1
+        }
+    },
+
+    on: {
+        slideChange: function () {
+          const wrapper = document.querySelector('.slider-wrapper');
+          if (this.activeIndex === 0) {
+            wrapper.style.backgroundColor = '#f6fc9f76'; // Background for Slide 1
+          } else if (this.activeIndex === 1) {
+            wrapper.style.backgroundColor = '#9ffcb376'; // Background for Slide 2
+          } else if (this.activeIndex === 2) {
+            wrapper.style.backgroundColor = '#99fdff76'; // Background for Slide 3
+          }
+        }
+      }
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+});
+  
