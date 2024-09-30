77cefb35e6f337858bfc8b4e2fb7a4500c683496
hrvyCased <salinas.johnharvey11@gmail.com>
2024-09-30 10:05:46 +0800

    Add CORS headers to API endpoint

--- api/chat.js
+++ api/chat.js
@@ -4,6 +4,10 @@ import axios from 'axios';
 
 // Export default handler function for the API route
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
   // Destructure message from request body
   const { message } = req.body;
 
